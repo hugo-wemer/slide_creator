5814464a8c207eacf40e3c1ee3da6af3996c7d35
hugo-wemer <wemer.eng@gmail.com>
2021-08-16 21:11:31 -0300

    Criação de slides funcionando. O próximo passo é reajustar visualmente e procurar possíveis bugs.

--- creator.py
+++ creator.py
@@ -1,14 +1,18 @@
 import os
 from pptx import Presentation
+from pptx.util import Pt
+
 
 def start():
     print("*********************************************")
     print("**Bem-vindo ao script de criação dos slides**")
     print("*********************************************")
 
+
 def files_verification():
     print("Coloque a música que deseja na mesma pasta desse script. Tecle 'Enter' ao finalizar")
     status = input("")
+
 
 def archive_list():
     global files, dir
@@ -17,6 +21,7 @@ def archive_list():
     for root, dirs, files in os.walk(dir):
         songs = [files]
     return files
+
 
 def select_song():
     global selected_file, complete_dir_file
@@ -27,43 +32,47 @@ def select_song():
         index += 1
     selected_index = int(input(""))
     selected_file = files[selected_index]
-    print("Você selecionou a música: {}". format(selected_file))
+    print("Você selecionou a música: {}".format(selected_file))
     complete_dir_file = dir + selected_file
+
 
 def read_lyrics():
     lyric = open(complete_dir_file, 'r')
-    index = 0
     prs = Presentation()
     title_slide_layout = prs.slide_layouts[0]
     slide = prs.slides.add_slide(title_slide_layout)
     title = slide.shapes.title
-    title.text = selected_file.replace(".txt","").replace("-"," ").upper()
-    paragraphy = []
-    inv_o = ""
+    title.text = selected_file.replace(".txt", "").replace("-", " ").upper()
+    i = 0
+    par = []
 
     for line in lyric:
         line = line.strip('\n')
-        print(line)
         if(line != ""):
-            inp = inv_o + '\n' + line
-            paragraphy.insert(index, inp)
+            if(i == 0):
+                paragraphy = line
+            else:
+                paragraphy = paragraphy + "\n" + line
         else:
-            index += 1
-        inv_o = line
-    print(paragraphy)
+            par.append(paragraphy)
+            paragraphy = ""
 
-    for par in paragraphy:
+        i += 1
+
+    par.append(paragraphy)
+
+    for ls in par:
+        #print(ls)
         lyric_slide = prs.slide_layouts[0]
         slide = prs.slides.add_slide(lyric_slide)
         title = slide.shapes.title
-        title.text = par
+        title.text = ls
+        print(ls)
 
+    #print(par)
     pptx_name = selected_file + ".pptx"
-    pptx_name = pptx_name.replace(".txt","")
+    pptx_name = pptx_name.replace(".txt", "")
     prs.save(pptx_name)
-
-
-
 
 
 
